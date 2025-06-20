81c8f2a45080053ab0d3998c153de5700ab8da0d
KelzangUD <kuding755@gmail.com>
2025-06-20 15:11:59 +0600

    Port U

--- data_analyst_api.py
+++ data_analyst_api.py
@@ -19,4 +19,4 @@ def get_data():
         return jsonify({"error": str(e)}), 500
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(host='0.0.0.0', port=5003, debug=True)
