1fe5c0fa31722ab6faa5299d743a381e5743d6eb
KelzangUD <kuding755@gmail.com>
2025-06-20 16:13:42 +0600

    sensor data summary api

--- data_analyst_api.py
+++ data_analyst_api.py
@@ -6,17 +6,49 @@ from flask_cors import CORS # Import CORS
 app = Flask(__name__)
 DATA_FILE = "data/iot_sensor_data.csv"
 # Initialize CORS
-# This will allow requests from 'http://localhost:3000' to any route in your Flask app.
-# You can specify methods if needed, e.g., methods=["GET", "POST"]
-CORS(app, resources={r"/data*": {"origins": "http://localhost:3000"}})
+CORS(app)
+CORS(app, origins="http://localhost:3000")
 
-# If you want to allow CORS for all origins (less secure, but quick for local development):
-# CORS(app)
+df = pd.read_csv(DATA_FILE)
 
-# If you want to allow CORS for all methods and headers from specific origin:
-# CORS(app, origins="http://localhost:3000")
+def clean_data(df):
+    expected_types = {
+        'Timestamp': 'datetime64[ns]',
+        'Temperature_C': 'float64',
+        'Humidity_Percent': 'float64',
+        'Light_Lux': 'float64',
+        'Motion_Detected': 'int64',
+        'Door_Open': 'int64',
+        'Power_Consumption_Watts': 'float64'
+    }
+    df_updated = df.copy()
+    # Convert mismatched columns
+    print("\n--- Initial Type Conversion Check ---")
+    for column, expected_type in expected_types.items():
+        actual_type = str(df_updated.dtypes.get(column))
+        if actual_type != expected_type:
+            try:
+                if "datetime" in expected_type:
+                    df_updated[column] = pd.to_datetime(df_updated[column], errors='coerce')
+                else:
+                    df_updated[column] = df_updated[column].astype(expected_type)
+            except Exception as e:
+                return f"Failed to convert '{column}': {e}"
 
+    if 'Timestamp' in df_updated.columns:
+        df_updated = df_updated.set_index('Timestamp')
+    else:
+        return "Warning: 'Timestamp' column not found to set as index. Is it already the index or named differently?"
 
+    nan_in_index = df_updated.index.isna().sum()
+
+    if nan_in_index > 0:
+        df_updated = df_updated[df_updated.index.notna()] 
+    else:
+        return "No NaT values found in the Timestamp index."
+
+    df_cleaned = df_updated.copy()
+    return df_cleaned
 
 #ensure the data directory exists
 if not os.path.exists(DATA_FILE):
@@ -26,8 +58,15 @@ if not os.path.exists(DATA_FILE):
 def get_data():
     """Fetch the IoT sensor data."""
     try:
-        df = pd.read_csv(DATA_FILE)
         return jsonify(df.to_dict(orient='records')), 200
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
+
+@app.route('/summary', methods=['GET'])
+def get_summary():
+    """Fetch Summary IoT sensor data."""
+    try:
+        return jsonify(clean_data(df).to_dict(orient='records')), 200
     except Exception as e:
         return jsonify({"error": str(e)}), 500
 
